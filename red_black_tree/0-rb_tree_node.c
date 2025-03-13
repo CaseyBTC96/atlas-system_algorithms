48c101241f5dff8c4309f4137db59c422e77d1f7
casey paul <casey.paul@atlasschool.com>
2025-03-12 19:11:51 -0500

    started task 0

---
+++ red_black_tree/0-rb_tree_node.c
@@ -0,0 +1,17 @@
+#include "rb_trees.h"
+
+/* malloc NULL */
+#include <stdlib.h>
+
+/* fprintf */
+#include <stdio.h>
+
+/**
+ * rb_tree_node - creates a red-black binary tree node of struct `rb_tree_t`
+ *
+ * @parent: pointer to parent of node created
+ * @value: integer stored in new node
+ * @color: red-black polarity of new node
+ * Return: pointer to new node
+ */
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
