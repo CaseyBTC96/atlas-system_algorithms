0addbb61a222f17cd4cbefcad7d11e6a245749d6
casey paul <casey.paul@atlasschool.com>
2025-03-19 19:47:21 -0500

    task 0

--- red_black_tree/0-rb_tree_node.c
+++ red_black_tree/0-rb_tree_node.c
@@ -15,3 +15,25 @@
  * Return: pointer to new node
  */
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
+{
+	rb_tree_t *new;
+
+	if (color > DOUBLE_BLACK || color < RED)
+	{
+		fprintf(stderr,
+			"rb_tree_node: color out of rb_color_t range\n");
+		return (NULL);
+	}
+
+	new = malloc(sizeof(rb_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->n = value;
+	new->color = color;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
