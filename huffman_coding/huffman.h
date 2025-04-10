ceeaf0f5f6b69a175e1227b6abc30ab242badac6
casey Paul <casey.paul@atlasschool.com>
2025-04-10 03:25:10 +0000

    tisk tisk 55

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -1,3 +1,12 @@
+#ifndef _HUFFMAN_H_
+#define _HUFFMAN_H_
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "heap/heap.h"
+
 /**
  * struct symbol_s - Stores a char and its associated frequency
  *
@@ -9,3 +18,16 @@ typedef struct symbol_s
     char data;
     size_t freq;
 } symbol_t;
+
+#define CHAR_TABLE_SIZE 256
+#define FREQ_TABLE_SIZE 256
+
+typedef char char_table_t[CHAR_TABLE_SIZE];
+typedef size_t freq_table_t[FREQ_TABLE_SIZE];
+
+extern char *program_invocation_name;
+extern char *program_invocation_short_name;
+
+symbol_t *symbol_create(char data, size_t freq);
+
+#endif /* _HUFFMAN_H_ */
