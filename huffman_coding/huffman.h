0a27c949b104cf5b628775949a8d82fc587da905
casey Paul <casey.paul@atlasschool.com>
2025-04-10 03:29:48 +0000

    tit for tat 6

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -29,5 +29,6 @@ extern char *program_invocation_name;
 extern char *program_invocation_short_name;
 
 symbol_t *symbol_create(char data, size_t freq);
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 
 #endif /* _HUFFMAN_H_ */
