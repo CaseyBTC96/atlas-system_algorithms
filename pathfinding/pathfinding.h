42564185aa6b459477de13a8052556e0601ac76a
casey.paul <casey.paul@atlasschool.com>
2025-04-17 02:53:12 +0000

    task 1

--- pathfinding/pathfinding.h
+++ pathfinding/pathfinding.h
@@ -64,6 +64,7 @@ queue_t *backtracking_array(char **map, int rows, int cols,
 			    point_t const *start, point_t const *target);
 
 /* 1-backtracking_graph.c */
-queue_t *backtracking_graph(graph_t *graph, vertex_t const *start, vertex_t const *target);
+queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
+	       		    vertex_t const *target);
 
 #endif /* PATHFINDING_H */
